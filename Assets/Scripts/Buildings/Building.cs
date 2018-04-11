55cb554437306ce9f11e9952e87a2061640080d0
The1Black\swan <swancd@gmail.com>
2018-04-12 00:17:31 +0200

    Scripts, Models, en Matirials toegevoegd
    -Een script voor "Building"
    -Een script voor de "Nodes" / targets
    -Een beetje de mappenstructuur veranderd voor mijn onderdeel
    -Een material voor zowel de "Building" als de "Nodes"
    - en als laatste het model en animatiecontroller toegevoegd

---
+++ Assets/Scripts/Buildings/Building.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    // This script is used too keep track of the buildings level of power and animate accordingly.
+
+
+    // _lvlOfPower is called this way because: it tracks the amount of "Power" the "building" has.
+    [SerializeField]
+    private float _lvlOfPower = 100;
+
+    public Color startColour;
+    public Color andColour;
+
+    public float LvlOfPower
+    {
+        get
+        {
+            return _lvlOfPower;
+        }
+        set
+        {
+            _lvlOfPower += value;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Renderer>().material.color = Color.Lerp(startColour, andColour, _lvlOfPower / 100);
+    }
+}
---
+++ Assets/Scripts/Buildings/Nodes/Node.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    // This script will act as a "Node" or "Target" and when it is hit it will "charge" the "Tower" or "Building" its connected to.
+
+    public GameObject parentBuilding;
+
+    // _nodeMaxEnergy is called this way because: it tracks the amount of "Power" it passes to the "building".
+    [SerializeField]
+    private float _nodeMaxEnergy = 25;
+
+
+    [SerializeField]
+    private float _currentNodeEnergy = 0;
+
+
+    [SerializeField]
+    private float _energyBoost = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        parentBuilding = GetComponentInParent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentNodeEnergy >= _nodeMaxEnergy)
+        {
+            Destroy(gameObject);
+        }
+        else if (_currentNodeEnergy <= _nodeMaxEnergy)
+        {
+
+        }
+    }
+}
