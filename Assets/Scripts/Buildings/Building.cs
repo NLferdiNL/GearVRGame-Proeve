48cf6fc41740ae08267da15cf20138cfcb71ca96
The1Black\swan <swancd@gmail.com>
2018-04-12 13:48:40 +0200

    Added my Buildings to Main
    -Recreated my scene

---
+++ Assets/Scripts/Buildings/Building.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    // This script is used too keep track of the buildings level of power and animate accordingly.
+
+    [SerializeField]
+    public Animator buildingAnimator;
+
+    // _lvlOfPower is called this way because: it tracks the amount of "Power" the "building" has.
+    [SerializeField]
+    private float _lvlOfPower = 100;
+
+    public Color startColour;
+    public Color andColour;
+
+    public float LvlOfPower
+    {
+        get
+        {
+            return _lvlOfPower;
+        }
+        set
+        {
+            _lvlOfPower += value;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (buildingAnimator == null)
+        {
+            buildingAnimator = GetComponentInParent<Animator>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Renderer>().material.color = Color.Lerp(startColour, andColour, _lvlOfPower / 100);
+        buildingAnimator.SetFloat("amountOfPower", _lvlOfPower);
+    }
+}
