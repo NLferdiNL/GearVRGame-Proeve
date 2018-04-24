e218c16b875b2034b665d8fe1bb921bcb2009ec1
Dylan Waij <d_way@live.nl>
2018-04-24 09:39:05 +0200

    Fix script
    - Raycast Laser greek semicolon zucht.

--- Assets/Scripts/Turret/RaycastLaser.cs
+++ Assets/Scripts/Turret/RaycastLaser.cs
@@ -1,28 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class RaycastLaser : MonoBehaviour {
 
     //This script creates the laser and the controls for space and touch.
 
-    [SerializeField] private float range; // Sets Range of Line.
+    public float range; // Sets Range of Line.
 
-    [SerializeField] private LayerMask hitMask;
+    [SerializeField]LayerMask hitMask;
 
-    private LineRenderer laserLine; // Line for the laser.
+    LineRenderer laserLine; // Line for the laser.
 
-    private bool isShooting = true;
+    bool isShooting = true;
 
     void Awake() // Gets the laserLine component and enables it true at start of the scene.
     {
-        laserLine = GetComponent<LineRenderer>();
-        laserLine.enabled = true;
-        range = 18;
+        laserLine = GetComponent<LineRenderer>();
+        laserLine.enabled = true;
+        range = 18;
+        //laserLine.SetWidth(0.1f, 0.25f);
+        //
     }
     void Update()
     {
-        TempControls();
+        TempControls();
     }
 
     void TempControls()
@@ -31,57 +33,60 @@ public class RaycastLaser : MonoBehaviour {
         {
             if (isShooting)
             {
-                DisableEffects();
+                DisableEffects();
             }
             else if (isShooting == false)
             {
-                EnableEffects();
+                EnableEffects();
             }
             if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
             {
                 if (isShooting)
                 {
-                    DisableEffects();
+                    DisableEffects();
                 }
                 else if (isShooting == false)
                 {
-                    EnableEffects();
+                    EnableEffects();
                 }
             }
         }
     }
 
-    void FixedUpdate() // Checks if person hit space or touched on phone and runs Shoot();
+    void FixedUpdate() // Checks if person hit space or touched on phone and runs Shoot();
     {
-        Shoot();
+        Shoot();
 
     }
     void DisableEffects() // Disables the laserLine.
     {
-        laserLine.enabled = false;
-        isShooting = false;
+        laserLine.enabled = false;
+        isShooting = false;
     }
     void EnableEffects() // Enables the laserLine.
     {
-        laserLine.enabled = true;
-        isShooting = true;
+        laserLine.enabled = true;
+        isShooting = true;
     }
 
     void Shoot() // Creates the line visable in game if laserLine.enabled = true.
     {
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
 
-        laserLine.SetPosition(0, transform.position);
+        laserLine.SetPosition(0, transform.position);
 
 
         if (Physics.Raycast(ray, out hit, range, hitMask))
         {
-            laserLine.SetPosition(1, hit.point);
-            hit.collider.gameObject.SendMessage("Heal", 0, SendMessageOptions.DontRequireReceiver);
+            laserLine.SetPosition(1, hit.point);
+            hit.collider.gameObject.SendMessage("Heal", 0, SendMessageOptions.DontRequireReceiver);
         }
         else {
-            laserLine.SetPosition(1, ray.origin + ray.direction * range);
+
+            //laserLine.enabled = true;
+
+            laserLine.SetPosition(1, ray.origin + ray.direction * range);
         }
     }
 
