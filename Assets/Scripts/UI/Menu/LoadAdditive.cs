af2ce2fea26818c5e1eee261b3897248cdb284cf
The1Black\swan <swancd@gmail.com>
2018-05-07 10:02:28 +0200

    Scripts for menu

---
+++ Assets/Scripts/UI/Menu/LoadAdditive.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAdditive : MonoBehaviour
+{
+    void LoadInNextScene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Additive);
+    }
+}
