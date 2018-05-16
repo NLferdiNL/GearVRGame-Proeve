a54f6885e6c8a4bcc92c19d73f430193c1616ab6
Ferdi <nlferdinl@gmail.com>
2018-05-16 13:02:48 +0200

    Removed some locks on AudioVisualization.

--- Assets/Scripts/Environment/Instantiate512Cubes.cs
+++ Assets/Scripts/Environment/Instantiate512Cubes.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class Instantiate512Cubes : MonoBehaviour {
     public GameObject SampleCubePrefab;
-    [SerializeField] 
+
     private GameObject[] sampleCube = new GameObject[512]; //Amount of cubes that scale with the audio for visual representation. Max amount of cubes you can put in is 512
     [SerializeField]
     private float maxScale;
     [SerializeField]
-    private float amountListeners = 512f; //this is where you can put in how many audiocubes you want, the max you can put in is 512 and the minimum is 0.
+    private int amountListeners = 512; //this is where you can put in how many audiocubes you want, the max you can put in is 512 and the minimum is 0.
     [SerializeField]
     private float circleDegree = 360f; //the degrees of how far you want it to span round in degrees. 360 for it to span a full circle, and 90 for example to span a quater of a circle
 
+	[SerializeField]
+	float cubeSize = 1;
+
 	// Use this for initialization
 	void Start () {
+		sampleCube = new GameObject[amountListeners];
+
 		for (int i = 0; i < amountListeners; i++)
         {
-            GameObject instanceSampleCube = (GameObject)Instantiate(SampleCubePrefab);
-            instanceSampleCube.transform.position = this.transform.position;
-            instanceSampleCube.transform.parent = this.transform;
+            GameObject instanceSampleCube = Instantiate(SampleCubePrefab);
+            instanceSampleCube.transform.position = transform.position;
+            instanceSampleCube.transform.parent = transform;
             instanceSampleCube.name = "AudioCube " + i; //Renames AudioCube along with the number it is
-            this.transform.eulerAngles = new Vector3(0, -(circleDegree / amountListeners) * i, 0); //This places all 512 AudioCubes in a circle
+			instanceSampleCube.transform.localScale = new Vector3(cubeSize, 1, cubeSize);
+            transform.eulerAngles = new Vector3(0, -(circleDegree / amountListeners) * i, 0); //This places all 512 AudioCubes in a circle
             instanceSampleCube.transform.position = Vector3.forward * 100;
             sampleCube[i] = instanceSampleCube;
         }
@@ -29,11 +35,13 @@ public class Instantiate512Cubes : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		for (int i=0; i<amountListeners; i++)
+		for (int i=0; i< sampleCube.Length; i++)
         {
             if (sampleCube != null)
             {
-                sampleCube[i].transform.localScale = new Vector3(1, (AudioVisualization.Samples[i] * maxScale) +2, 1);  
+                sampleCube[i].transform.localScale = new Vector3(sampleCube[i].transform.localScale.x,
+																 (AudioVisualization.Samples[i] * maxScale) +2, 
+																 sampleCube[i].transform.localScale.z);  
             }
         }
 	}
