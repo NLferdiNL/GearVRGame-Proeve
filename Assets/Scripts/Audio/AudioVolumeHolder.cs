a5a9b746d415e4aaf80542aeb237145a54f09df2
The1Black\swan <swancd@gmail.com>
2018-05-17 01:00:48 +0200

    VolumeChager gemaakt foor het menu

---
+++ Assets/Scripts/Audio/AudioVolumeHolder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVolumeHolder : MonoBehaviour
+{
+    private static int musicVolume;
+    private static int sfxVolume;
+
+    public int MusicVolume
+    {
+        get
+        {
+            return musicVolume;
+        }
+        set
+        {
+            value = musicVolume;
+        }
+    }
+
+    public int SfxVolume
+    {
+        get
+        {
+            return sfxVolume;
+        }
+        set
+        {
+            value = sfxVolume;
+        }
+    }
+}
