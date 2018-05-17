f58b73ac31149f190ce68e7b073b8a500b3a6cf1
Ferdi <nlferdinl@gmail.com>
2018-05-17 11:36:17 +0200

    New drone model, some audio to animation test

--- Assets/Scripts/Audio/AudioData.cs
+++ Assets/Scripts/Audio/AudioData.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class AudioData : MonoBehaviour {
 
+	[Header("MUST BE A POWER OF TWO!")]
+	public int audioRange = 256;
+
+	static AudioData instance;
+
+	AudioSource audioSource;
+
+	float[] audioSpectrumData;
+
 	// Use this for initialization
 	void Start () {
-		
+		audioSource = GetComponent<AudioSource>();
+
+		audioSpectrumData = new float[audioRange];
+		instance = this;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void FixedUpdate() {
+		instance.audioSource.GetSpectrumData( instance.audioSpectrumData, 0, FFTWindow.Blackman);
+	}
+
+	public static float GetFloat(int range) {
+		return instance.audioSpectrumData[range];
 	}
 }
