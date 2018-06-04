e07e1dde4722ff6da4fa13b52a2190640e3c1f46
Dylan Waij <d_way@live.nl>
2018-06-04 15:34:27 +0200

    Veranderingen sfx en soundmanager

--- Assets/Scripts/Audio/SoundController.cs
+++ Assets/Scripts/Audio/SoundController.cs
@@ -1,58 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class SoundController : MonoBehaviour {
 
-    [SerializeField] private SoundManager SM;
+    [SerializeField] private SoundManager SM; // Hold the SoundManager.
 
-    public UnityEvent OnReset = new UnityEvent();
+    public UnityEvent OnReset = new UnityEvent(); // Reset the buildings from true to false and start second section.
+    public UnityEvent NumberChange = new UnityEvent(); // Change in number notification.
+    public UnityEvent StartEndless = new UnityEvent();
+    public static SoundController Instance;
 
-    public UnityEvent NumberChange = new UnityEvent();
-
-    public bool TutorialBuilding1, TutorialBuilding2, TutorialBuilding3; // Check of 
+    public bool TutorialBuilding1, TutorialBuilding2, TutorialBuilding3; // Buildings checks.
 
     // [0] = start song || [5] = start mid section || [13] = start drop || [15] = start end loop.
 
     void Start()
     {
-        IntroSection();
+        IntroSection(); // Start the first section of the game.
     }
 
-    public void CheckBuildingCharge(int BuildingNumber)
+    public void CheckBuildingCharge(int BuildingNumber) // Set buildings to true if charged.
     {
         switch (BuildingNumber)
         {
             case 1:
-                TutorialBuilding1 = true;
+                TutorialBuilding1 = true; // Sets the First building to true.
                 break;
             case 2:
-                TutorialBuilding2 = true;
+                TutorialBuilding2 = true; // Sets the Second building to  true.
                 break;
             case 3:
-                TutorialBuilding3 = true;
+                TutorialBuilding3 = true; // Sets the Third building to true.
                 break;
         }
     }
 
     void IntroSection()
     {
-        StartCoroutine(IntroStart());
+        StartCoroutine(IntroStart()); // Start Ienumerator intro.
     }
-    IEnumerator IntroStart()
+    IEnumerator IntroStart() // 
     {
-        SM.MusicPlayer.clip = SM.MusicHolder[0];
-        SM.MusicPlayer.PlayOneShot(SM.MusicPlayer.clip, 1f); // FadeInto
-        yield return new WaitForSeconds(SM.MusicPlayer.clip.length);
-        NumberChange.Invoke();
+        SM.MusicPlayer.clip = SM.MusicHolder[0]; // Set audiosource clip from music holder.
+        SM.MusicPlayer.PlayOneShot(SM.MusicPlayer.clip, 1f); // FadeInto clip.
+        yield return new WaitForSeconds(SM.MusicPlayer.clip.length); // Wait until clip is done then continue.
+        NumberChange.Invoke(); // Invoke Number change for (Blank)
         /*if (TutorialBuilding1 == false)
         {
             BuildingNotCharged();
-        }*/
+        }*/ // If the building is not charged game over screen.
         SM.MusicPlayer.clip = SM.MusicHolder[1];
-        SM.MusicPlayer.PlayOneShot(SM.MusicPlayer.clip, 1f); // FadeInto2
+        SM.MusicPlayer.PlayOneShot(SM.MusicPlayer.clip, 1f); // FadeInto2 clip.
         yield return new WaitForSeconds(SM.MusicPlayer.clip.length);
         NumberChange.Invoke();
         /*if (TutorialBuilding2 == false)
@@ -60,17 +62,17 @@ public class SoundController : MonoBehaviour {
             BuildingNotCharged();
         }*/
         SM.MusicPlayer.clip = SM.MusicHolder[2];
-        SM.MusicPlayer.PlayOneShot(SM.MusicPlayer.clip, 1f); // FadeIntoVocals
+        SM.MusicPlayer.PlayOneShot(SM.MusicPlayer.clip, 1f); // FadeIntoVocals clip.
         yield return new WaitForSeconds(SM.MusicPlayer.clip.length);
         NumberChange.Invoke();
         /*if (TutorialBuilding3 == false)
         {
             BuildingNotCharged();
         }*/
-        TutorialBuilding1 = TutorialBuilding2 = TutorialBuilding3 = false;
-        OnReset.Invoke();
+        TutorialBuilding1 = TutorialBuilding2 = TutorialBuilding3 = false; // Set buildings to false for the second phase.
+        OnReset.Invoke(); // Invoke event for buildings.
 
-        InBetweenSection();
+        InBetweenSection(); // Start the inbetween section of the game.
     }
 
     void BuildingNotCharged()
@@ -80,19 +82,19 @@ public class SoundController : MonoBehaviour {
 
     void InBetweenSection()
     {
-        StartCoroutine(InBetween());
+        StartCoroutine(InBetween()); // Start Ienumerator of in between section.
     }
     IEnumerator InBetween()
     {
         SM.MusicPlayer.clip = SM.MusicHolder[3];
-        SM.MusicPlayer.Play(); // BuildupInto
+        SM.MusicPlayer.Play(); // BuildupInto clip.
         yield return new WaitForSeconds(SM.MusicPlayer.clip.length);
         NumberChange.Invoke();
         SM.MusicPlayer.clip = SM.MusicHolder[4];
-        SM.MusicPlayer.Play(); // Drop
+        SM.MusicPlayer.Play(); // Drop clip.
         yield return new WaitForSeconds(SM.MusicPlayer.clip.length);
         NumberChange.Invoke();
-        MidSection();
+        MidSection(); // Start the mid section of the game.
     }
 
     void MidSection()
@@ -145,6 +147,7 @@ public class SoundController : MonoBehaviour {
     }
     IEnumerator SurvivalStart()
     {
+        StartEndless.Invoke();
         SM.MusicPlayer.clip = SM.MusicHolder[8];
         SM.MusicPlayer.Play(); // MidBuild
         yield return new WaitForSeconds(SM.MusicPlayer.clip.length);
--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -4,21 +4,21 @@ using UnityEngine.Events;
 public class SoundManager : MonoBehaviour
 {
 
-    [SerializeField] private  AudioClip[] sfx;
-    public AudioClip[] SfxHolder
+    [SerializeField] private  AudioClip[] sfx; // Hold sfx clips.
+    public AudioClip[] SfxHolder // allow clip usage for other scripts.
     {
         get { return sfx; }
     }
 
     //public UnityEvent SetSfx = new UnityEvent();
 
-    [SerializeField] private AudioClip[] music;
-    public AudioClip[] MusicHolder
+    [SerializeField] private AudioClip[] music; // Hold the music clips.
+    public AudioClip[] MusicHolder // Send music clips to other scripts.
     {
         get { return music; }
         set { music = value; }
     }
 
-    public AudioSource MusicPlayer;
+    public AudioSource MusicPlayer; // Play the music one at a time.
 
 }
